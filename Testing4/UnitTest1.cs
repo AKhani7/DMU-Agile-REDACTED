528e84231d561d380181b51ced8d1f720c0d2a21
Tommy Vu <P2603594@my365.dmu.ac.uk>
2022-02-10 23:05:27 +0000

    yes testing idk

---
+++ Testing4/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing4
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {  //create an instance of the class we want to create
+            clsOrder anOrder = new clsOrder();
+            //test to see that it exists 
+            Assert.IsNotNull(anOrder);
+        }
+    }
+}
