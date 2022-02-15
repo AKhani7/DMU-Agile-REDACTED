667550fe7fada927898ee69915cc42e47c5b7b8e
UnchartedWorld <nathangvjcollins@gmail.com>
2022-02-15 22:49:41 +0000

    Provided CSS file for staff, modified ASPX for Staff pages and provided basic textbox functionality.

--- AdminSystem/StaffList.aspx
+++ AdminSystem/StaffList.aspx
@@ -4,7 +4,9 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <title></title>
+    <link href="~/StaffImagesAndCSS/Staff.css" rel="stylesheet" type="text/css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Staff List Page</title>
 </head>
 <body>
     <form id="form1" runat="server">
--- AdminSystem/StaffViewer.aspx.cs
+++ AdminSystem/StaffViewer.aspx.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ClassLibrary;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,11 @@ public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        // Creates a new instance of the class.
+        clsStaff staff = new clsStaff();
+        // Retrieves the data from the session object.
+        staff = (clsStaff)Session["sampleStaffData"];
+        // Displays the staff role on the page (hopefully).
+        Response.Write(staff.StaffNo  + "\n" + staff.StaffName + "\n" + staff.StaffRole + "\n" + "£" + staff.StaffSalary + "\n" + staff.DateAdded + "\n" + staff.IsEmployed);
     }
 }
